861d028d7bca2254aba907ca704aebd27b747c30
yay4ya <yay4ya.dev@gmail.com>
2025-05-10 17:11:32 +0900

    update schedule url

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,1 @@
-export const SCHEDULE_URL =
-  "https://gist.githubusercontent.com/yay4ya/223a7744bc0003e4dcef84b60cd9352f/raw/07691e9fd4fdc05a7a1abc1b4670112efbeae9cd/oming.json";
+export const SCHEDULE_URL = "https://raw.githubusercontent.com/yay4ya/oming/refs/heads/schedule/schedule.json";
