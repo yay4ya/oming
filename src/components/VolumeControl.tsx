cf6b736232990ab464ce3802c52b713abc698153
yay4ya <yay4ya.dev@gmail.com>
2025-05-10 22:42:02 +0900

    modify volume control

--- src/components/VolumeControl.tsx
+++ src/components/VolumeControl.tsx
@@ -13,16 +13,10 @@ function VolumeControl({ player, ...props }: React.HTMLProps<HTMLDivElement> & {
     }
   }, [player]);
 
-  const handleVolumeChange = React.useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      const newVolume = Number.parseInt(event.target.value, 10);
-      setVolume(newVolume);
-      if (player?.g) {
-        player.setVolume(newVolume);
-      }
-    },
-    [player],
-  );
+  const handleVolumeChange = React.useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    const newVolume = Number.parseInt(event.target.value, 10);
+    setVolume(newVolume);
+  }, []);
 
   const VolumeIcon = React.useMemo(() => {
     if (mute) return VolumeXIcon;
@@ -44,6 +38,12 @@ function VolumeControl({ player, ...props }: React.HTMLProps<HTMLDivElement> & {
       else player.unMute();
     }
   }, [player, mute]);
+
+  React.useEffect(() => {
+    if (player?.g) {
+      player.setVolume(volume);
+    }
+  }, [player, volume]);
 
   return (
     <div {...props}>
